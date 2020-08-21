0cb84fda916a6b53e66637726144a66f7ab51928
Viktor Soshko <Lorgar4@ukr.net>
2020-08-21 15:03:31 +0300

    Добавлена модель для генерации html блоков

---
+++ src/model.js
@@ -0,0 +1,27 @@
+export const model =  [
+    {
+    type: 'title',
+    value: 'Test Title',
+    options: {
+
+        }
+    },
+    {
+        type: 'text',
+        value: 'Text content',
+        options: {
+    
+            }
+    },
+    {
+        type: 'textColums',
+        value: [
+            '1 text',
+            '2 text',
+            '3 text'
+        ],
+        options: {
+    
+            }
+    }
+]
