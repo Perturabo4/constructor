f9396f1a198052cf6b28f026dcf46a4d318ed604
Viktor Soshko <Lorgar4@ukr.net>
2020-08-21 15:48:46 +0300

    Добавлен файл utils.js для вспомогательных функций

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import {model} from './model';
-import {title, text, textColumns} from './templates';
+import {templates} from './templates';
 import './css/main.css';
 
 const site = document.querySelector('#site');
 
 model.forEach( block => {
-    let html;
-    if(block.type === 'title') {
-        html = title(block);
-    } else if(block.type === 'text') {
-        html = text(block);
-    } else if(block.type === 'textColumns') {
-        html = textColumns(block);
-    }
+    const generate = templates[block.type];
 
-    site.insertAdjacentHTML('beforeend', html);
+    if(generate) {
+        const html = generate(block);
+        site.insertAdjacentHTML('beforeend', html);
+    }
+    
 })
 
--- src/templates.js
+++ src/templates.js
@@ -1,28 +1,24 @@
-export function title(block) {
-    return `
-    <div class="row">
-        <div class="col-sm">
-            <h1>${block.value}</h1>
-        </div>
-    </div>
-    `;
+import { row, col } from "./utils";
+
+function title(block) {
+    return row (
+        col(`<h1>${block.value}</h1>`)
+    );
 }
 
-export function text(block) {
-    return `
-    <div class="row">
-        <div class="col-sm">
-            <p>${block.value}</p>
-        </div>
-    </div>
-    `;
+function text(block) {
+    return row(
+            col(`<p>${block.value}</p>`)
+    );
 }
 
-export function textColumns(block) {
-    const html = block.value.map( item => `<div class="col-sm">${item}</div>`)
-    return `
-    <div class="row">
-        ${html.join("")}
-    </div>
-    `;
+function textColumns(block) {
+    const html = block.value.map( item => col(item))
+    return row(html.join(""));
+}
+
+export const templates = {
+    title,
+    text,
+    textColumns
 }
